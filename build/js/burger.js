c3e925ded593c59065d6c079afc7ecee22924f8e
Itina Winter <lobanova.irina.96@mail.ru>
2020-08-29 02:21:17 +0300

    Устанавлвает корректные пути для изображений

--- build/js/burger.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var burger = document.querySelector('.burger');
-  var mobileMenu = document.querySelector('.header__main-nav-mobile-menu');
-
-  burger.addEventListener('click', function () {
-    burger.classList.toggle("open");
-    mobileMenu.classList.toggle("menu");
-  });
-})();
